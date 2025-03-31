047aed1a4eea72ebcb3e19264c27b14b10a67d8a
ewhalgand <halgandewen44@gmail.com>
2025-03-31 15:37:13 +0200

    feat: update package & added exemple.env

--- models/productModel.js
+++ models/productModel.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
 const productSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    body: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
 
-
-module.exports = mongoose.model("Products", productSchema)
+module.exports = mongoose.model("Products", productSchema);
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 const productsRoute = require("./routes/products");
 
 const app = express();
 
 /* Middleware */
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+/* Home */
+app.get("/", (req, res) => {
+  res.send(`<h3 style="font-family: sans-serif">My CRUD API !</h3>`);
+});
 
 /* Routes */
-app.use('/api/products', productsRoute)
-
+app.use("/api/products", productsRoute);
 
 /* Connect to DB */
 mongoose
